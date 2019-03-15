7771fbc0288de8aa29f788d45502e97c74cc3a7b
Anhar Athoi <anharzihan@gmail.com>
2019-03-15 11:07:10 +1100

    make movie, user and rating instances

--- main.rb
+++ main.rb
@@ -12,8 +12,17 @@ class Movie
 end
 
 class User
+  attr_accessor :id
   def initialize(id)
     @id = id
+  end
+end
+
+class Rating
+  def initialize(user, movie)
+    @user = user
+    @movie = movie
+    @id = "#{user.id}_#{movie.id}"
   end
 end
 
@@ -25,9 +34,58 @@ movie_instances = []
 
 ALL_MOVIES.each do |lines|
   movie_instance = Movie.new(lines[:movieid], lines[:title])
-  # p lines[:title]
+  # p lines
   movie_instances << movie_instance
 end
+# p movie_instances
+################### ALL users and ratings ######################
+ALL_USERS = CSV.read('./ratings.csv', {headers: true, header_converters: :symbol})
+
+all_user_entries = []
+all_rating_entries = []
+
+ALL_USERS.each do |lines|
+  # saves all the users from the ratings db
+  all_user_entries << lines[:userid]
+
+  # saves all the entries of ratings
+  all_rating_entries << lines
+end
+
+unique_user_ids = all_user_entries.uniq
+
+######## All user instances ########
+unique_user_instances = unique_user_ids.map do |user_id|
+   User.new(user_id)
+end
+# p unique_users_instances.length
+# p unique_user_instances[0].id
+
+######## All rating instances ########
+# at the moment it's taking about a minute
+# p all_rating_entries.length
+# begin_time = Time.now
+# puts "================================="
+# puts "begin #{begin_time}"
+
+rating_instances = all_rating_entries.map do |rating_entries|
+  begin_time = Time.now
+  # find the user instance
+  user_id = rating_entries[:userid]
+  user_instance = unique_user_instances.detect {|user_instance| user_instance.id == user_id}
+
+  # find the movie instance
+  movie_id = rating_entries[:movieid]
+  movie_instance = movie_instances.detect {|movie_instance| movie_instance.id == movie_id}
+  # Rating.new
+  Rating.new(user_instance, movie_instance)
+end
+end_time = Time.now
+# puts "================================="
+# puts "end #{end_time}"
+# puts "================================="
+# # p rating_instances[0..10]
+# puts "time taken fo rating instances = #{(end_time - begin_time)}"
 
 ################### Top rated movies ######################
 MOST_RATED = File.read('./most_rated_movies.json')
@@ -46,4 +104,5 @@ top_rated_100_movies = JSON.parse(MOST_RATED, {:symbolize_names => true})
 
 # p movie_instances
 
-p movie_instances[0].title
+# p movie_instances[0].title
+# p movie_instances[1].title
